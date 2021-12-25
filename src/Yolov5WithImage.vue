f8aac640f1d9f6445d5b123dea2391314a55680b
Wentao KONG 孔文涛 <wentao.kong@MBC02FM01TMD6V.local>
2021-12-25 23:11:34 +0800

    tab seperated

---
+++ src/Yolov5WithImage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <img class="cam" :src="images[0]" />
+  </div>
+</template>
+<script>
+import img1 from "./assets/1.jpeg";
+
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+}
+
+export default {
+  data() {
+    return {
+      images: [img1],
+    };
+  },
+  mounted() {
+    this.images = [img1];
+  },
+  methods: {
+    async initModal() {},
+  },
+};
+</script>
+<style>
+.cam {
+  width: 300px;
+  height: 300px;
+}
+</style>
